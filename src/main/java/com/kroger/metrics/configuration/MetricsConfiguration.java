f87aed92825a8efbba2c29ac9fd0d72cbb879640
chandanam-kr <chandana.madyala@kroger.com>
2026-06-12 14:53:16 +0530

    Spring 3 version

--- src/main/java/com/kroger/metrics/configuration/MetricsConfiguration.java
+++ src/main/java/com/kroger/metrics/configuration/MetricsConfiguration.java
@@ -11,8 +11,8 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.micrometer.metrics.autoconfigure.MeterRegistryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
--- src/test/java/com/kroger/metrics/configuration/MetricsConfigurationTest.java
+++ src/test/java/com/kroger/metrics/configuration/MetricsConfigurationTest.java
@@ -7,7 +7,7 @@ import io.micrometer.core.instrument.config.MeterFilter;
 import io.micrometer.core.instrument.config.MeterFilterReply;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.micrometer.metrics.autoconfigure.MeterRegistryCustomizer;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 
 import java.util.List;
 import java.util.Map;
